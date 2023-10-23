ec7227f46b76369449639c201a4fb35e90bff39a
nahyun0121 <knh4769@khu.ac.kr>
2023-10-23 17:36:57 +0900

    feat: SecurityConfig

---
+++ src/main/java/ETA/whats_your_eta/api/config/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ETA.whats_your_eta.api.config.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/api/user").permitAll()
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+        return http.build();
+    }
+}
