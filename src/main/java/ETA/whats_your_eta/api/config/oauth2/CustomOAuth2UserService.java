f397550459340e3506a33c39a69c89fe78bb7f7d
nana <knh4769@khu.ac.kr>
2023-11-12 23:56:12 +0900

    fix: OAuth 수정

--- src/main/java/ETA/whats_your_eta/api/config/oauth2/CustomOAuth2UserService.java
+++ src/main/java/ETA/whats_your_eta/api/config/oauth2/CustomOAuth2UserService.java
@@ -1,10 +1,10 @@
-package ETA.whats_your_eta.api.config.auth;
+package ETA.whats_your_eta.api.config.oauth2;
 
-import ETA.whats_your_eta.api.config.auth.dto.OAuthAttributes;
-import ETA.whats_your_eta.api.config.auth.dto.SessionUser;
 import ETA.whats_your_eta.api.domain.user.User;
 import ETA.whats_your_eta.api.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -13,38 +13,37 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
-    private final HttpSession httpSession;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 기본 OAuth2UserService 객체 생성 후 이를 사용하여 OAuth2User 정보를 가져온다.
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-        // OAuth Service Id (Google, Kakao)
+        // OAuth Service ID (Google, Kakao)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
         // OAuth 로그인 진행 시 PK. 네이버랑 카카오는 기본 지원 X. 얘네랑 구글 로그인 같이 쓸 때 사용
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
+        log.info(registrationId);
+        log.info(userNameAttributeName);
+
+        // OAuth2User 정보로 OAuth2Attribute 객체 생성
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-        User user = saveOrUpdate(attributes);
+        log.info(String.valueOf(attributes));
 
-        httpSession.setAttribute("user", new SessionUser(user));
+        // OAuth2Attribute의 속성값들을 Map으로 반환 받는다.
+        Map<String, Object> mapAttribute = attributes.convertToMap();
 
-        return new DefaultOAuth2User(user.getAuthorities(), attributes.getAttributes(), attributes.getNameAttributeKey());
-    }
-
-    private User saveOrUpdate(OAuthAttributes attributes) {
-        User user = userRepository.findByEmail(attributes.getEmail())
-                .map(entity -> entity.update(attributes.getName()))
-                .orElse(attributes.toEntity());
-
-        return userRepository.save(user);
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")), mapAttribute, "email");
     }
 }
--- src/main/java/ETA/whats_your_eta/api/config/oauth2/OAuthAttributes.java
+++ src/main/java/ETA/whats_your_eta/api/config/oauth2/OAuthAttributes.java
@@ -1,28 +1,22 @@
-package ETA.whats_your_eta.api.config.auth.dto;
+package ETA.whats_your_eta.api.config.oauth2;
 
 import ETA.whats_your_eta.api.domain.user.Role;
 import ETA.whats_your_eta.api.domain.user.User;
 import lombok.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ToString
+@Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
     private String nameAttributeKey;
     private String name;
     private String email;
+    private String provider;
 
-    @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email) {
-        this.attributes = attributes;
-        this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
-        this.email = email;
-    }
-
-    //public static class OAuthAttributesBuilder {}
 
     public static OAuthAttributes of(String provider, String userNameAttributeName, Map<String, Object> attributes) {
         switch (provider) {
@@ -56,12 +50,21 @@ public class OAuthAttributes {
                 .build();
     }
 
-    // OAuth에서 엔티티를 생성하는 시점: 처음 가입할 때
-    public User toEntity() {
-        return User.builder()
-                .name(name)
-                .email(email)
-                .role(Role.USER)
-                .build();
+//    // OAuth에서 엔티티를 생성하는 시점: 처음 가입할 때
+//    public User toEntity() {
+//        return User.builder()
+//                .name(name)
+//                .email(email)
+//                .role(Role.USER)
+//                .build();
+    Map<String, Object> convertToMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", nameAttributeKey);
+        map.put("key", nameAttributeKey);
+        map.put("name", name);
+        map.put("email", email);
+        map.put("provider", provider);
+
+        return map;
     }
 }
